f8ac47b5b27f9b1c330e62385136f6b4999b0896
Andrew Sutedja <andrew.sutedja@gmail.com>
2016-07-11 17:18:30 -0700

    Finish implementing getElementsbyClassName and start on parseJson

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,22 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+//Reading up on what an Element and a Node is and what methods are available is important.
+var getElementsByClassName = function(className) {
+	var doc=document;
+	var elements = [];
+	var recur = function(node,func){
+		func(node);
+		node = node.firstChild;
+		while(node){
+			recur(node,func);
+			node = node.nextSibling
+		}
+	}
+	recur(doc, function(node){
+		if(node.classList && node.classList.contains(className)){
+			elements.push(node)
+		}
+	});
+	return elements;
 };
--- src/parseJSON.js
+++ src/parseJSON.js
@@ -3,5 +3,69 @@
 
 // but you're not, so you'll write it from scratch:
 var parseJSON = function(json) {
+  var pos;
+  var char;
+  //function used to go to next character in json
+  var next = function(){
+  	pos += 1;
+  	char = json.charAt(at);
+  	return char;
+  };
+  //function used to determine what type of value/collection it is.  
+  var result = function(){
+  	if(char && char >=0 && char <=9 || char === '-'){
+  		return number();
+  	}
+  	else if(char === '['){
+  		return array();
+  	}
+  	else if(char === '{'){
+  		return object();
+  	}
+  	else if(char === '\"'){
+  		return string();
+  	}
+  	else if (char === 't' || char === 'f' || char === 'n'){
+  		return boolean();
+  	}
+  }
+
+  //function used to determine which boolean or if null
+  var boolean = function(){
+  	var bool = '';
+  	if(char === 't'){
+  		for(var i = 0;i<4;i++){
+  			bool += char;
+  			next();
+  		}
+  		if(bool === 'true'){
+  			return true;
+  		}
+  	}
+   	else if(char === 'f'){
+  		for(var i = 0;i<5;i++){
+  			bool += char;
+  			next();
+  		}
+  		if(bool === 'false'){
+  			return false;
+  		}
+  	}
+  	else if(char === 'n'){
+  		for(var i = 0;i<4;i++){
+  			bool += char;
+  			next();
+  		}
+  		if(bool === 'null'){
+  			return null;
+  		}
+  	}
+  }
+
+  var number = function() {
+  	
+  }
+
+
   // your code goes here
 };
