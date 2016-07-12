139a0657a105427a1dc8a6ee75c1048bc704e5cb
Andrew Sutedja <andrew.sutedja@gmail.com>
2016-07-11 23:58:02 -0700

    Complete stringify and almost complete parse

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -7,65 +7,187 @@ var parseJSON = function(json) {
   var char;
   //function used to go to next character in json
   var next = function(){
-  	pos += 1;
-  	char = json.charAt(at);
-  	return char;
+    pos += 1;
+    char = json.charAt(pos);
+    return char;
+  };
+  var white = function(){
+    if (char && char === ' '){
+      next();
+    }
   };
   //function used to determine what type of value/collection it is.  
   var result = function(){
-  	if(char && char >=0 && char <=9 || char === '-'){
-  		return number();
-  	}
-  	else if(char === '['){
-  		return array();
-  	}
-  	else if(char === '{'){
-  		return object();
-  	}
-  	else if(char === '\"'){
-  		return string();
-  	}
-  	else if (char === 't' || char === 'f' || char === 'n'){
-  		return boolean();
-  	}
+    white();
+    if(char && char >=0 && char <=9 || char === '-'){
+      return number();
+    }
+    else if(char === '['){
+      return array();
+    }
+    else if(char === '{'){
+      return object();
+    }
+    else if(char === '\"'){
+      return string();
+    }
+    else if (char === 't' || char === 'f' || char === 'n'){
+      return boolean();
+    }
   }
 
   //function used to determine which boolean or if null
   var boolean = function(){
-  	var bool = '';
-  	if(char === 't'){
-  		for(var i = 0;i<4;i++){
-  			bool += char;
-  			next();
-  		}
-  		if(bool === 'true'){
-  			return true;
-  		}
-  	}
-   	else if(char === 'f'){
-  		for(var i = 0;i<5;i++){
-  			bool += char;
-  			next();
-  		}
-  		if(bool === 'false'){
-  			return false;
-  		}
-  	}
-  	else if(char === 'n'){
-  		for(var i = 0;i<4;i++){
-  			bool += char;
-  			next();
-  		}
-  		if(bool === 'null'){
-  			return null;
-  		}
-  	}
+    var bool = '';
+    if(char === 't'){
+      for(var i = 0;i<4;i++){
+        bool += char;
+        next();
+      }
+      if(bool === 'true'){
+        return true;
+      }
+    }
+    else if(char === 'f'){
+      for(var i = 0;i<5;i++){
+        bool += char;
+        next();
+      }
+      if(bool === 'false'){
+        return false;
+      }
+    }
+    else if(char === 'n'){
+      for(var i = 0;i<4;i++){
+        bool += char;
+        next();
+      }
+      if(bool === 'null'){
+        return null;
+      }
+    }
   }
 
   var number = function() {
-  	
+    var num  = '';
+    if(char === '-'){
+      num = '-';
+      next();
+    }
+
+    while(char && char >=0 && char <=9){
+      num += char;
+      next();
+    }
+
+        if (char === ".") {
+            num += ".";
+            while (next() && char >= "0" && char <= "9") {
+                num += char;
+            }
+        }
+    return +num;
+
+  }
+  var weird = {
+        "\"": "\"",
+        "\\": "\\",
+        "/": "/",
+        b: "\b",
+        f: "\f",
+        n: "\n",
+        r: "\r",
+        t: "\t"
+    };
+
+  var string = function(){
+    var str = '';
+    if(char === '\"'){
+      while(next()){
+        if(char === '\"'){
+          next();
+          return str;
+        }
+        // Accounts for weird \ cases like \\\ which is really \
+        if(char === '\\'){
+          next();
+          if(typeof weird[char] === 'string'){
+            str += weird[char];
+          }
+        }
+        else{
+          str += char;
+        }
+      }
+    }
   }
 
+ var array = function(){
+    var arr = [];
+    if(char === '['){
+      next();
+      white();
+      if(char === ']'){
+        next();
+        return arr;
+      }
 
-  // your code goes here
+      while(char){
+        arr.push(result());
+        white();
+        if(char === ']'){
+          next();
+          return arr;
+        }
+        next();
+        white();
+      }
+    }
+  } 
+
+  var object = function() {
+    var obj = {};
+    if(char === '{'){
+      next();
+      white();
+      if (char === '}'){
+        next();
+        return obj;
+      }
+    }
+    while(char){
+      var key = string();
+      console.log(key);
+      white();
+      next();
+      obj[key] = result();
+      white();
+      if(char === '}'){
+        next();
+      return obj;
+      }
+      next();
+      white(); 
+    }
+
+  }
+
+  pos = 0;
+  char = json.charAt(pos);
+  return result();
 };
+
+var a =     '{ "firstName": "John", "lastName" : "Smith", "age" : ' +
+    '25, "address" : { "streetAddress": "21 2nd Street", ' +
+    '"city" : "New York", "state" : "NY", "postalCode" : ' +
+    ' "10021" }, "phoneNumber": [ { "type" : "home", ' +
+    '"number": "212 555-1234" }, { "type" : "fax", ' +
+    '"number": "646 555-4567" } ] }'
+console.log(JSON.parse(a));
+console.log("Ours")
+console.log(parseJSON(a));
+/*var b = JSON.parse(a);
+var c = parseJSON(a);
+if (b === c){
+  console.log('correct');
+}*/
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,48 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  var result = '';
+  var check = typeof obj;
+  if (check === 'undefined' || check === 'symbol' || check === 'function') {// for undefined values
+    result += 'undefined'; 
+  } 
+  else if (check === 'number' || check === 'boolean') { //for numbers or booleans
+    result += obj;
+  } 
+  else if (check === 'string') { //for strings
+    result = result + '"' + obj + '"';
+  } 
+  else if (check === 'object') {
+    if(obj === null){//for null
+      return 'null';
+    }
+    else if(Array.isArray(obj)){//for arrays
+      result += '[';
+      for(var i=0;i<obj.length;i++){
+        result+= stringifyJSON(obj[i]);
+        if(i !== obj.length - 1){
+          result += ',';
+        }
+      }
+    result += ']';
+    }
+  	else { //for objects
+  	  result = result + '{';
+  	  for (var key in obj) {
+
+  	  	if (stringifyJSON(obj[key]) !== 'undefined') {
+  	  	  result = result + '"' + key + '"' + ':' + stringifyJSON(obj[key]) + ',';
+        }   
+  	  }
+  	  if (result[result.length-1] === ',') {
+  	    result = result.slice(0,result.length-1);
+  	  }
+  	  result = result + '}';
+      
+
+    }
+  }
+
+  return result;
+
 };
